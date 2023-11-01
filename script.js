0d41929ee9d8ebcb26388e5e841afe39312a537e
evilalienware <144330903+evilalienware@users.noreply.github.com>
2023-11-01 21:33:29 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// script.js
-
-window.onload = function() {
+function startMeme() {
     let memeAudio = document.getElementById('https://github.com/evilalienware/evilalienware.github.io/raw/main/Delinquent-Habits-Return-of-the-Tres.mp3');
     memeAudio.play();
+    
+    // Optionally, hide the start button after it's clicked.
+    let startButton = document.getElementById('startButton');
+    startButton.style.display = 'none';
 }
