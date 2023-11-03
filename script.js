fc0afd6b399858a3bce560a618623680f4369bed
evilalienware <144330903+evilalienware@users.noreply.github.com>
2023-11-03 20:20:02 +1100

    Update script.js

--- script.js
+++ script.js
@@ -3,51 +3,21 @@ let globalImage; // Store the loaded image globally so we can access it on slide
 function handleImage() {
     let input = document.getElementById('imageInput');
     let canvas = document.getElementById('canvas');
-    let ctx = canvas.getContext('2d');
 
     let file = input.files[0];
     let reader = new FileReader();
 
     reader.onload = function(e) {
-        let img = new Image();
-        img.src = e.target.result;
+        globalImage = new Image();
+        globalImage.src = e.target.result;
 
-        img.onload = function() {
-            const maxWidth = 1280; // Set the max width for the image
-            const maxHeight = 720; // Set the max height for the image
-
-            let width = img.width;
-            let height = img.height;
-
-            // Check if the image size exceeds the set dimensions and scale down proportionally
-            if (width > maxWidth) {
-                const ratio = maxWidth / width; // calculate ratio to scale image
-                height *= ratio;
-                width *= ratio;
-            }
-
-            if (height > maxHeight) {
-                const ratio = maxHeight / height; // calculate ratio to scale image
-                width *= ratio;
-                height *= ratio;
-            }
-
-            canvas.width = width;
-            canvas.height = height;
-
-            // Draw and pixelate the image
-            ctx.drawImage(img, 0, 0, width, height); // Draw the full-sized image
-            let pixelatedImage = new Image();
-            pixelatedImage.src = canvas.toDataURL();
-            pixelatedImage.onload = function() {
-                ctx.drawImage(pixelatedImage, 0, 0, width, height);
-            }
+        globalImage.onload = function() {
+            processImage(); // Process the image when it's loaded
         }
     }
 
     reader.readAsDataURL(file);
 }
-
 
 function processImage() {
     let canvas = document.getElementById('canvas');
