06492a2afacd0110a4cfc8aa8e00e3873169cbb4
evilalienware <144330903+evilalienware@users.noreply.github.com>
2023-11-03 18:03:10 +1100

    Update script.js

--- script.js
+++ script.js
@@ -11,12 +11,13 @@ function handleImage() {
         img.src = e.target.result;
 
         img.onload = function() {
-            const maxWidth = 1280; 
-            const maxHeight = 720; 
+            const maxWidth = 1280; // Set the max width for the image
+            const maxHeight = 720; // Set the max height for the image
 
             let width = img.width;
             let height = img.height;
 
+            // Check if the image size exceeds the set dimensions
             if (width > maxWidth) {
                 height = (maxWidth / width) * height;
                 width = maxWidth;
@@ -30,6 +31,7 @@ function handleImage() {
             canvas.width = width;
             canvas.height = height;
 
+            // Draw and pixelate the image
             ctx.drawImage(img, 0, 0, width / 8, height / 8);
             let pixelatedImage = new Image();
             pixelatedImage.src = canvas.toDataURL();
